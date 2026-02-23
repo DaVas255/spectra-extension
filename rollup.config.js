a284e74541bc96c2d547a3cb6e3d113ab513063e
Danil Vasilchikov <DaVas255@gmail.com>
2026-02-24 02:52:42 +0700

    fix: manifest and rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,48 +3,53 @@ import commonjs from '@rollup/plugin-commonjs'
 import typescript from '@rollup/plugin-typescript'
 import copy from 'rollup-plugin-copy'
 
+const plugins = [
+  resolve({ browser: true }),
+  commonjs(),
+  typescript({ tsconfig: './tsconfig.json' })
+]
+
 export default [
-	{
-		input: 'src/background/background.ts',
-		output: {
-			file: 'dist/background.js',
-			format: 'esm'
-		},
-		plugins: [
-			resolve({ browser: true }),
-			commonjs(),
-			typescript({ tsconfig: './tsconfig.json' })
-		]
-	},
-	{
-		input: 'src/content/content.ts',
-		output: {
-			file: 'dist/content.js',
-			format: 'iife'
-		},
-		plugins: [
-			resolve({ browser: true }),
-			commonjs(),
-			typescript({ tsconfig: './tsconfig.json' })
-		]
-	},
-	{
-		input: 'src/popup/popup.ts',
-		output: {
-			file: 'dist/popup.js',
-			format: 'iife'
-		},
-		plugins: [
-			resolve({ browser: true }),
-			commonjs(),
-			typescript({ tsconfig: './tsconfig.json' }),
-			copy({
-				targets: [
-					{ src: 'src/popup/popup.html', dest: 'dist' },
-					{ src: 'manifest.json', dest: 'dist' },
-					{ src: 'icons', dest: 'dist' }
-				]
-			})
-		]
-	}
+  {
+    input: 'src/background/background.ts',
+    output: {
+      file: 'dist/background.js',
+      format: 'esm'
+    },
+    plugins: [
+      ...plugins,
+      copy({
+        targets: [
+          { src: 'src/popup/popup.html', dest: 'dist' },
+          { src: 'manifest.json', dest: 'dist' },
+          { src: 'icons', dest: 'dist' }
+        ],
+        hook: 'buildStart'
+      })
+    ]
+  },
+  {
+    input: 'src/injected/injected.ts',
+    output: {
+      file: 'dist/injected.js',
+      format: 'iife'
+    },
+    plugins
+  },
+  {
+    input: 'src/content/content.ts',
+    output: {
+      file: 'dist/content.js',
+      format: 'iife'
+    },
+    plugins
+  },
+  {
+    input: 'src/popup/popup.ts',
+    output: {
+      file: 'dist/popup.js',
+      format: 'iife'
+    },
+    plugins
+  }
 ]
